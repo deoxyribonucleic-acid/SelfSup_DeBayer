9824b5615523dba54b7deaadbdfcc4d4d17723a0
mikaeru11 <mikaeru@umich.edu>
2025-03-18 15:06:16 -0400

    Add files via upload
    Preprocessing: Bayer Filter and Interpolation
    Metrics: PNSR & SSIM

---
+++ test.py
@@ -0,0 +1,22 @@
+import cv2
+import preprocessing as pp
+import metrics as mt
+
+path = 'kodim01.png'
+
+img = cv2.imread(path)
+
+bayer = pp.bayer_filter(path)
+
+rec = pp.interp(bayer)
+
+psnr = mt.PSNR(img, rec)
+print(f"PSNR value is {psnr} dB")
+ssim = mt.SSIM(img, rec)
+print(f"SSIM value is {ssim}")
+
+cv2.imshow('original', img)
+cv2.imshow('bayer filter', bayer)
+cv2.imshow('reconstruction', rec)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
